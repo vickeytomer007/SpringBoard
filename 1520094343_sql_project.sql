65b2a9bfd66da805d4405bf401d2d45a1259a746
Vivek Kumar <vickeytomer@gmail.com>
2019-04-09 00:15:51 +0530

    Update 1520094343_sql_project.sql

--- 1520094343_sql_project.sql
+++ 1520094343_sql_project.sql
@@ -27,11 +27,11 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
 
-SELECT  * FROM FACILITIES WHERE MEMBERCOST<>0
+SELECT  name FROM FACILITIES WHERE MEMBERCOST<>0
 
 /* Q2: How many facilities do not charge a fee to members? */
 
-SELECT  * FROM FACILITIES WHERE MEMBERCOST=0
+SELECT  COUNT(name) AS zero_membercost FROM FACILITIES WHERE MEMBERCOST=0
 
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
@@ -55,14 +55,16 @@ SELECT  * FROM FACILITIES WHERE facid in (1,5)
 more than $100? Return the name and monthly maintenance of the facilities
 in question. */
 
-SELECT  *,case when monthlymaintenance>100 then 'expensive' else 'cheap' end as Labelled FROM FACILITIES WHERE 1=1 
+SELECT  name,case when monthlymaintenance>100 then 'expensive' else 'cheap' end as Labelled FROM FACILITIES WHERE 1=1 
 
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Do not use the LIMIT clause for your solution. */
 
 SELECT  top 1 firstname,surname FROM MEMBERS order by joindate desc
-
+>or
+SELECT     firstname, surname FROM    members WHERE    joindate = (SELECT 
+            MAX(joindate)    FROM   members);
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
 Include in your output the name of the court, and the name of the member
@@ -84,36 +86,56 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
-SELECT distinct name as [facility name],firstname+' '+surname as [member name],membercost,slots  FROM MEMBERS MB
-JOIN BOOKINGS BS ON MB.MEMID=BS.MEMID
-JOIN FACILITIES FS ON FS.FACID=BS.FACID
-where 1=1 
-and convert(date,starttime,102)='2012-09-14'
-and membercost>30 or guestcost>30 
-and mb.memid=0
-order by membercost desc
+SELECT     f.name AS facility_name,
+    CONCAT(m.firstname, ' ', m.surname) AS member_ID,
+    CASE
+        WHEN m.memid != 0 THEN b.slots * f.membercost
+        WHEN m.memid = 0 THEN b.slots * f.guestcost
+    END AS cost
+FROM   members m
+        JOIN    bookings b ON m.memid = b.memid
+        JOIN    facilities AS f ON b.facid = f.facid
+WHERE    b.starttime >= '2012-09-14'
+        AND b.starttime < '2012-09-15'
+        AND ((m.memid != 0
+        AND b.slots * f.membercost > 30)
+        OR (m.memid = 0
+        AND b.slots * f.guestcost > 30))
+ORDER BY cost DESC;
 
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
-SELECT distinct name as [facility name],firstname+' '+surname as [member name],membercost,slots  FROM MEMBERS MB
-JOIN BOOKINGS BS ON MB.MEMID=BS.MEMID
-JOIN FACILITIES FS ON FS.FACID=BS.FACID
-where 1=1 
-and convert(date,starttime,102)='2012-09-14'
-and membercost>30 or guestcost>30 
-and mb.memid=0
-order by membercost desc
+SELECT     sub.name AS facility_name,
+    sub.member AS member_ID,
+    sub.cost AS cost
+FROM    (SELECT 
+        CONCAT(m.firstname, ' ', m.surname) AS member,
+            f.name,
+            CASE
+                WHEN m.memid != 0 THEN b.slots * f.membercost
+                WHEN m.memid = 0 THEN b.slots * f.guestcost
+            END AS cost
+    FROM        members m
+    JOIN bookings b ON m.memid = b.memid
+    JOIN facilities f ON b.facid = f.facid
+    WHERE        b.starttime >= '2012-09-14'
+            AND b.starttime < '2012-09-15'
+    HAVING cost > 30) AS sub
+ORDER BY cost DESC;
 
 
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
-
-
-SELECT name,sum(membercost+guestcost+initialoutlay-monthlymaintenance) as'total revenue'  FROM MEMBERS MB
-JOIN BOOKINGS BS ON MB.MEMID=BS.MEMID
-JOIN FACILITIES FS ON FS.FACID=BS.FACID
-where 1=1 
-group by name
-having sum(membercost+guestcost+initialoutlay-monthlymaintenance)<1000
-order by [total revenue]
+SELECT     facility_name, revenue AS total_revenue
+FROM    (SELECT 
+        SUM(CASE
+                WHEN b.memid != 0 THEN b.slots * f.membercost
+                WHEN b.memid = 0 THEN b.slots * f.guestcost
+            END) AS revenue,
+            f.name AS facility_name
+    FROM        bookings b
+    JOIN facilities f ON b.facid = f.facid
+    GROUP BY f.name
+    HAVING revenue < 1000) AS sub
+ORDER BY revenue;
